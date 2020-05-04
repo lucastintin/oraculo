3c8b9e7e8725b2853e0350d305ed3c8803398aa0
Lucas Amiune <lucastintin@gmail.com>
2020-05-03 23:51:34 -0300

    Ajustando a var do Mongo ENV

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MONGODB_URI   : process.env.MONGO_URI || "mongodb://localhost:27017/oraculoDB",
+    MONGODB_URI   : process.env.MONGODB_URI || "mongodb://localhost:27017/oraculoDB",
     PORT        : process.env.PORT || 5000
 }
