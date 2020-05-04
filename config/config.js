789358cdb4d58be14f022081f407320eef6eca8e
Lucas Amiune <lucastintin@gmail.com>
2020-05-03 23:49:01 -0300

    Corrigindo a VAR do MongoDB

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MONGO_URI   : process.env.MONGO_URI || "mongodb://localhost:27017/oraculoDB",
+    MONGODB_URI   : process.env.MONGO_URI || "mongodb://localhost:27017/oraculoDB",
     PORT        : process.env.PORT || 5000
 }
--- database/database.js
+++ database/database.js
@@ -2,7 +2,7 @@ const config = require('./../config/config');
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(config.MONGO_URI, { useNewUrlParser: true , useUnifiedTopology: true }, (err) => {
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true , useUnifiedTopology: true }, (err) => {
     if (err) {
         console.log('ERRO: Conexão com banco de dados. - ' + err);
     }
